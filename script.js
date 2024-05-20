bfeeae022ab6b3bc803fc137988ab15cede7bf71
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-20 21:09:09 +0530

    added a async function to get products data

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const url = "https://fakestoreapi.com/products";
+const getProducts = async () => {
+  const url = "https://fakestoreapi.com/products";
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+};
+getProducts();

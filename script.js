07f0e6d03f81ffbdf28df2fb5c7417a396a56d50
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-20 21:24:55 +0530

    added map method on data to get all elements

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const getProducts = async () => {
   const url = "https://fakestoreapi.com/products";
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
+  //   console.log(data);
+  let results = ""
+  data.map((result)=>{
+    results = ``
+  })
 };
 getProducts();

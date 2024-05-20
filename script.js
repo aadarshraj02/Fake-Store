9c24a37339bc82db48382b9cd38f497d263ec97b
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-20 21:31:40 +0530

    added html to store all data

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const getProducts = async () => {
   const response = await fetch(url);
   const data = await response.json();
   //   console.log(data);
-  let results = ""
-  data.map((result)=>{
-    results = ``
-  })
+  let results = "";
+  data.map((result) => {
+    results = `<div class="card">
+    <h1 class="title">${result.title}</h1>
+    <img class="images" src=${result.image} alt="img" />
+    <p>${result.description}</p>
+    <p class="category">${result.category}</p>
+    <p class="price">${result.price}$</p>
+  </div>`;
+  });
+  console.log(results);
 };
 getProducts();
